18116121229850916f5bcfd4e787e9ff3b4b34f1
gensokyoz1 <gensokyochen29@gmail.com>
2026-05-23 22:10:53 +0200

    started login page, barely

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,36 @@
 function Login () {
 
     return (
+        <section 
+        style = {{
+            backgroundColor: "black",
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        }}> 
 
-        <div>
+        <div
+        style = {{
+        border: "2px solid white", 
+        width: "90%",
+        height: "80%",
+        }}
+        
+            >
+                <div
+                style = {{
+                    height: "100%",
+                    width: "60%",
+                    border: "2px solid white", 
+                }} > 
+                    <h1> Get Started Now</h1>
+                </div>
 
-            <h1>
-                Login!
-            </h1>
+                        
         </div>
+        </section>
     );
 }
 
